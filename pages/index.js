9b81f6752a9cdd7105594c808da3f518693a6ac1
Buster Senior <121201452+BusterSR@users.noreply.github.com>
2023-01-28 21:22:58 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <img src="/controller.svg" className={styles.icon} width="800" width="800"/>
+        <img src="/controller.svg" className={styles.icon} />
         <h3>Enter keywords for things you want in your generated ideas</h3>
         <form onSubmit={onSubmit}>
           <input
