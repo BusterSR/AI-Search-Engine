e744d4b072bec5328e4858b6cfbbcef4d84fe563
Buster Senior <121201452+BusterSR@users.noreply.github.com>
2023-01-28 20:18:55 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>What type of game do you want?</h3>
+        <h3>What genre of game do you want ideas for?</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
