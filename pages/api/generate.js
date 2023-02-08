a0dc4cb8b141bfda2ad959ae9b69f12d736b353e
Buster Senior <121201452+BusterSR@users.noreply.github.com>
2023-02-07 19:45:06 -0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -30,7 +30,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       //this is where i may have messed up
-      temperature: 0.6,
+      temperature: 0.9,
     });
     res.status(200).json({ result: completion.data.choices[0].text});
   } catch(error) {
